b910a1384c3c4a7dc10693a2b64a3e65e4f9d4b4
Info <info@zoga.me>
2019-04-08 17:18:23 +0900

    modified Database

--- Language/ja/Database.php
+++ Language/ja/Database.php
@@ -15,13 +15,14 @@
  */
 
 return [
-   'invalidEvent'          => '{0} is not a valid Model Event callback.',
-   'invalidArgument'       => 'You must provide a valid {0}.',
-   'invalidAllowedFields'  => 'Allowed fields must be specified for model: {0}',
-   'emptyDataset'          => 'There is no data to {0}.',
-   'failGetFieldData'      => 'Failed to get field data from database.',
-   'failGetIndexData'      => 'Failed to get index data from database.',
-   'failGetForeignKeyData' => 'Failed to get foreign key data from database.',
-   'parseStringFail'       => 'Parsing key string failed.',
-   'featureUnavailable'    => 'This feature is not available for the database you are using.',
+   'invalidEvent'          => '{0} は有効なモデルイベントコールバックではありません。', // '{0} is not a valid Model Event callback.',
+   'invalidArgument'       => '{0} は有効な引数ではありません。', // 'You must provide a valid {0}.',
+   'invalidAllowedFields'  => '許可フィールドは、モデルを指定する必要があります： {0}', // 'Allowed fields must be specified for model: {0}',
+   'emptyDataset'          => '{0} にデータがありません。', // 'There is no data to {0}.',
+   'failGetFieldData'      => 'データベースからのフィールドデータの取得に失敗しました。', // 'Failed to get field data from database.',
+   'failGetIndexData'      => 'データベースからのインデックスデータの取得に失敗しました。', // 'Failed to get index data from database.',
+   'failGetForeignKeyData' => 'データベースからの外部キーのデータの取得に失敗しました。', // 'Failed to get foreign key data from database.',
+   'parseStringFail'       => 'キーの文字列の解析に失敗しました。', // 'Parsing key string failed.',
+   'featureUnavailable'    => 'この機能は、使用しているデータベースでは利用できません。', // 'This feature is not available for the database you are using.',
 ];
+
