5fe7d2341830d4705021361ae416279651f5601f
Info <info@zoga.me>
2019-04-09 15:55:51 +0900

    Update View

--- Language/ja/View.php
+++ Language/ja/View.php
@@ -14,10 +14,10 @@
  */
 
 return [
-   'invalidCellMethod'     => '{class}::{method} is not a valid method."',
-   'missingCellParameters' => '{class}::{method} has no params.',
-   'invalidCellParameter'  => '{0} is not a valid param name.',
-   'noCellClass'           => 'No view cell class provided.',
-   'invalidCellClass'      => 'Unable to locate view cell class: {0}.',
-   'tagSyntaxError'        => 'You have a syntax error in your Parser tags: {0}',
+   'invalidCellMethod'     => '{class}::{method} は有効なメソッドではありません。', // '{class}::{method} is not a valid method."',
+   'missingCellParameters' => '{class}::{method} のパラメータがありません', // '{class}::{method} has no params.',
+   'invalidCellParameter'  => '{0} は有効なパラメータではありません', // '{0} is not a valid param name.',
+   'noCellClass'           => 'View ライブラリクラスが提供されていません。', // 'No view cell class provided.',
+   'invalidCellClass'      => 'View ライブラリクラスの場所が無効です： {0}', // 'Unable to locate view cell class: {0}.',
+   'tagSyntaxError'        => 'パーサータグの構文エラーです： {0}', // 'You have a syntax error in your Parser tags: {0}',
 ];
